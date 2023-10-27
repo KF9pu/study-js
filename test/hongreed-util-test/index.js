d97d55c62fb2d559c75650f112a05d4d8acffd58
hong seong hwa <pushan45hsh@gmail.com>
2023-10-27 12:51:03 +0900

    feat : 테스트로 사용했던 코드 제거 및 utils 새로 작성

---
+++ test/hongreed-util-test/index.js
@@ -0,0 +1,6 @@
+import { add, divide, multiply, subtract } from "hogreed-utils";
+// -> 가져온 함수들을 사용하고 결과를 출력한다.
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
---
+++ test/hongreed-util-test/node_modules/hogreed-utils/src/index.cjs
@@ -0,0 +1,19 @@
+// src/index.cjs
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
